b879858dcaf264f2189e4e5ca895db74e13d929b
Babur Shah  Sayer <babursayer@gmail.com>
2022-08-17 14:13:14 +0430

    fix Card

---
+++ src/components/Card.vue
@@ -0,0 +1,42 @@
+<template>
+  <li class="card" @click="flipCard">
+    <div v-show="!flipped" class="card-front">{{ card.front}} </div>
+    <div v-show="flipped" class="card-back">{{ card.back }} </div>
+    <button @click="$emit('deleteCard', card.id)">Delete</button>
+  </li>
+</template>
+
+<script>
+
+export default {
+  props: ["card"],
+  emits: ["deleteCard"],
+  methods: {
+    flipCard(event) {
+      event.stopPropagation();
+      this.flipped = !this.flipped;
+    }
+  },
+  data(){
+    return {
+      flipped: false
+    }
+  }
+}
+</script>
+
+
+
+<style scoped lang="scss">
+  .card {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.5em;
+    border: 1px solid black;
+    height: 250px;
+    width: 200px; 
+    border-radius: 3px;
+    margin-left: 30px;
+  }
+</style>
